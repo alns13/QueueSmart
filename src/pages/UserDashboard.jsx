334044adff7390d9e9c83399ab2513aa0f33c918
alns13 <ahsu1336@gmail.com>
2026-07-06 11:08:34 -0500

    added tailwindcss and shadcn

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -7,7 +7,7 @@ export default function UserDashboard() {
       <Navbar />
       <main style={{ marginLeft: '240px', padding: '20px', width: '100%' }}>
         <h1>User Dashboard</h1>
-        <p>This is placeholder text, I'll replace this later with something... i dont know yet. </p>
+          
       </main>
     </div>
   );
