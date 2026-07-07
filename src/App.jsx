4417784faaee0d98ed7ac833ca14c0ef5f84c44b
SenLiang Deng <tommy360928994@gmail.com>
2026-07-06 22:54:33 -0500

    added Administrator screens(Dashboard,Queue management,Service management and Report)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import UserDashboard from "./pages/UserDashboard.jsx";
+import Admindashbord from "./pages/administrator/Admin_dashboard.jsx";
+import Queue_Manage from "./pages/administrator/Queue_Management.jsx";
+import Server_manage from "./pages/administrator/Server_Management.jsx"
+import Report from "./pages/administrator/Admin_Report.jsx"
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/user-dashboard" element={<UserDashboard />} />
+      <Route path="/Admin_dashboard" element={<Admindashbord />}/>
+      <Route path="/Queue_Management" element={<Queue_Manage />}/>
+      <Route path="/Server_management" element={<Server_manage />}/>
+      <Route path="/Admin_Report" element={<Report />}/>
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
